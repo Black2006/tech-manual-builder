25e4cf0ed02d956738ae1270ffe22d73086a9cc8
VirtualFoxR7 <158249273+VirtualFoxR7@users.noreply.github.com>
2025-11-19 02:46:18 -0400

    Update ContentArea.tsx

--- src/components/ContentArea.tsx
+++ src/components/ContentArea.tsx
@@ -39,6 +39,9 @@ const sectionImages: Record<string, string> = {
 const institutionImages = [universidadJardin, universidadEntrada];
 const equiposInstitucionImages = [equipoInstitucion1, equipoInstitucion2];
 
+const equipo-desarrollo = [equipoInstitucion1, equipoInstitucion2];
+
+
 const ContentArea = ({ activeSection }: ContentAreaProps) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState("");
